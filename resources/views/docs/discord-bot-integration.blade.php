c4a615bebca18185eeab74793814c89ebad77d6d
Gage B. <gagebarbour96@gmail.com>
2024-03-20 21:13:27 -0500

    Finished Sidebar

--- resources/views/docs/discord-bot-integration.blade.php
+++ resources/views/docs/discord-bot-integration.blade.php
@@ -18,84 +18,8 @@
     </header>
 
     <main class="relative flex justify-center mx-auto max-w-8xl sm:px-2 lg:px-8 xl:px-12">
-        <label
-            class="fixed bottom-0 left-0 z-50 flex items-center justify-center w-12 h-12 mb-4 ml-4 bg-white border rounded-full shadow-lg cursor-pointer text-slate-600 border-slate-300 lg:hidden transition duration-200 ease-in-out active:scale-95"
-            for="navigation">
-            <svg class="w-6 h-6" fill="none" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg">
-                <path d="M4 8h16M4 16h16" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>
-        </label>
 
-        {{-- <div
-            class="fixed top-[3.5rem] h-screen shadow-xl px-4 left-0 hidden peer-checked:block lg:relative lg:top-0 lg:h-auto lg:px-0 lg:block lg:flex-none lg:shadow-none">
-            <div class="absolute inset-y-0 right-0 w-full lg:w-[50vw] bg-white lg:bg-slate-50"></div>
-
-            <nav class="sticky top-[4.5rem] w-64 pr-8 text-base lg:text-sm xl:w-72 xl:pr-16">
-                <ul class="-ml-0.5 h-[calc(100vh-4.5rem)] overflow-y-auto py-7 pl-0.5 space-y-8" role="list">
-                    <li>
-                        <h3 class="font-semibold tracking-tight text-slate-900">
-                            Getting started
-                        </h3>
-
-                        <ul class="pl-3 mt-3 space-y-2" role="list">
-                            <li>
-                                <a class="text-slate-900 hover:text-slate-800" href="#">
-                                    Quick start guide
-                                </a>
-                            </li>
-
-                            <li>
-                                <a class="text-slate-600 hover:text-slate-800" href="#">
-                                    How does Spinal work?
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li>
-                        <h3 class="font-semibold tracking-tight text-slate-900">
-                            Content Types
-                        </h3>
-
-                        <ul class="pl-3 mt-3 space-y-2" role="list">
-                            <li>
-                                <a class="text-slate-600 hover:text-slate-800" href="#">
-                                    What are content types?
-                                </a>
-                            </li>
-
-                            <li>
-                                <a class="text-slate-600 hover:text-slate-800" href="#">
-                                    Create and edit content types
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li>
-                        <h3 class="font-semibold tracking-tight text-slate-900">
-                            Content
-                        </h3>
-
-                        <ul class="pl-3 mt-3 space-y-2" role="list">
-                            <li>
-                                <a class="text-slate-600 hover:text-slate-800" href="#">
-                                    What kind of content can I create and edit?
-                                </a>
-                            </li>
-
-                            <li>
-                                <a class="text-slate-600 hover:text-slate-800" href="#">
-                                    Previewing content
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-        </div> --}}
-
+        @include('inc.docs.sidebar')
         <div class="flex-auto max-w-3xl min-w-0 px-4 py-10 mx-auto lg:pr-0 lg:pl-8 xl:px-16 text-white">
             <article class="">
                 <header class="">
