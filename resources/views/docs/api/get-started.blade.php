6eb90aae9db8ad13c18c37c00ce2e22e55c213be
Gage B. <gagebarbour96@gmail.com>
2024-04-03 18:34:44 -0500

    Fixed spelling and added flag about update time

--- resources/views/docs/api/get-started.blade.php
+++ resources/views/docs/api/get-started.blade.php
@@ -48,8 +48,30 @@
 
                     <p class="">
                         API Token can be found in the <span class="text-red-500 font-bold">Admin > Cad Settings > API
-                            Key</span>. The key is only availble for owners to reset and find.
+                            Key</span>. If you do not see a key
+                        there then click the green button that says "Generate/Generate API Key".
                     </p>
+
+                    <div class="rounded-md bg-yellow-50 p-4 my-3">
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <svg aria-hidden="true" class="h-5 w-5 text-yellow-400" fill="currentColor"
+                                    viewBox="0 0 20 20">
+                                    <path clip-rule="evenodd"
+                                        d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
+                                        fill-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="ml-3">
+                                <div class="text-sm text-yellow-700">
+                                    <p>There is a known issue that will not allow the key to show up immediately. Once
+                                        clicking the button wait 5 seconds then refresh the page and it will be there. DO
+                                        NOT CLICK THE BUTTON OVER AND OVER.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
                 <div class="space-y-3" id="step2">
